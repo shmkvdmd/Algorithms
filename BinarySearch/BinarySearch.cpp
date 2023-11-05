fa75dd1b21aeb10c6826a7be73b2002a6a8a5f69
shmkvdmd <demidka100@mail.ru>
2023-11-05 17:38:33 +0300

    Добавьте файлы проекта.

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+template<typename T>
+int binary_search(const std::vector<T> vec, int guessNumber)
+{
+	int min = 0;
+	int max = vec.size() - 1;
+	while (min <= max)
+	{
+		int mid = (min + max) / 2;
+		T currentGuess = vec[mid];
+		if (currentGuess == guessNumber)
+		{
+			return mid;
+		}
+		if (currentGuess > guessNumber)
+		{
+			max = mid - 1;
+		}
+		else if (currentGuess < guessNumber)
+		{
+			min = mid + 1;
+		}
+	}
+	std::cout << "Not found ";
+	return -1;
+}
+int main()
+{
+	std::vector<int> vec = { 5,6,7,8,9,10 };
+	std::cout << binary_search(vec, 8) << std::endl;
+	std::cout << binary_search(vec, 2) << std::endl;
+}
