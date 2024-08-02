11133fbadd6caa7a218175e58eb16dcc6a8c4706
shmkvdmd <101982070+shmkvdmd@users.noreply.github.com>
2024-08-02 19:35:05 +0300

    Update InsertionSort.cpp

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -13,10 +13,30 @@ void InsertionSort(std::vector<int>& vec){
     }
 }
 
-int main(){
-    std::vector<int> vec{5,2,4,6,1,3};
-    InsertionSort(vec);
+void ReverseInsertionSort(std::vector<int>& vec){
+    for(size_t j = 1; j != vec.size(); ++j){
+        int key = vec[j];
+        int i = j - 1;
+        while(i >= 0 && vec[i] < key){
+            vec[i + 1] = vec[i];
+            i = i - 1;
+        }
+        vec[i + 1] = key;
+    }
+}
+
+void PrintVector(std::vector<int> vec){
+    std::cout << std::endl;
     for (const auto& elem : vec){
         std::cout << elem << " ";
     }
+    std::cout << std::endl;
+}
+
+int main(){
+    std::vector<int> vec{5,2,4,6,1,3};
+    InsertionSort(vec);
+    PrintVector(vec);
+    ReverseInsertionSort(vec);
+    PrintVector(vec);
 }
